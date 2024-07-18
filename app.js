b41efe038b5abc52ad724e21abe58acaa9c66a90
Guilherme Vieira <guivipaiva@gmail.com>
2024-07-18 14:41:38 -0300

    projeto-numero-secreto

---
+++ app.js
@@ -0,0 +1,70 @@
+let listaDeNumerosSorteados = [];
+let numeroLimite = 10;
+let numeroSecreto = gerarNumeroAleatorio();
+let tentativas = 1;
+
+function exibirTextoNaTela(tag, texto){
+    let campo = document.querySelector(tag);
+    campo.innerHTML = texto;
+    responsiveVoice.speak(texto, 'Brazilian Portuguese Female', 
+    {rate:1.2});
+}
+
+function exibirTextoInicial(){
+    exibirTextoNaTela('h1', 'Jogo do número secreto');
+    exibirTextoNaTela('p', 'Escolha um número entre 1 e 10');
+}
+
+exibirTextoInicial();
+
+function verificarChute(){
+    let chute = document.querySelector('input').value;
+
+    if (chute == numeroSecreto) {
+        exibirTextoNaTela('h1', 'Acertou!');
+        let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa';
+        let mensagemTentativas = `Você descobriu o número secreto com ${tentativas} ${palavraTentativa}`; 
+        exibirTextoNaTela('p', mensagemTentativas);
+        document.getElementById('reiniciar').removeAttribute('disabled');
+        document.getElementById('botao-chute').setAttribute('disabled', 'true');
+    } else {
+        if (chute > numeroSecreto){
+            exibirTextoNaTela('p', 'O número é menor!');
+        } else {
+            exibirTextoNaTela('p', 'O número é maior!');
+        }
+        tentativas++;
+        limparCampo();
+    }   
+}
+
+function gerarNumeroAleatorio() {
+    let numeroEscolhido = parseInt(Math.random() * numeroLimite + 1);
+    let quantidadeDeElementosNaLista = listaDeNumerosSorteados.length;
+
+    if (quantidadeDeElementosNaLista == numeroLimite) {
+        listaDeNumerosSorteados = [];
+    }
+
+    if (listaDeNumerosSorteados.includes(numeroEscolhido)) {
+        return gerarNumeroAleatorio();
+    } else {
+        listaDeNumerosSorteados.push(numeroEscolhido);
+        return numeroEscolhido;
+    }
+}
+
+function limparCampo(){
+    chute = document.querySelector('input');
+    chute.value = '';
+}
+
+function reiniciarJogo(){
+    exibirTextoInicial();
+    numeroSecreto = gerarNumeroAleatorio();
+    limparCampo();
+    tentativas = 1;
+    document.getElementById('reiniciar').setAttribute('disabled', 'true');
+    document.getElementById('botao-chute').removeAttribute('disabled');
+}
+
